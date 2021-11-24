c4323bde8869165967ca037c11252bc3f62635d1
Christos Malamas <christosmalamas@outlook.com>
2021-11-24 11:54:11 +0200

    Updated token

--- src/settings.js
+++ src/settings.js
@@ -2,9 +2,9 @@ import { createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
 
 const appId = "a33def4bb6e34b8b8fed8d03f3e8e1e0";
 const token =
-  "006a33def4bb6e34b8b8fed8d03f3e8e1e0IAD5iurOuzio2r3IAdRryG13IkQgyXBDxc7NPFDgo+Pgr2TNKL8AAAAAEACdnafADGmeYQEAAQAMaZ5h";
+  "006a33def4bb6e34b8b8fed8d03f3e8e1e0IACVoL9Khw2Ye79n6lp/w290h+Ta8HK1pxR5S6tLDXsXawx+f9gAAAAAEACdnafAzVyfYQEAAQDNXJ9h";
 
 export const config = { mode: "rtc", codec: "vp8", appId: appId,token: token  };
 export const useClient = createClient(config);
 export const useMicrophoneAndCameraTracks = createMicrophoneAndCameraTracks();
-export const channelName = "main";
+export const channelName = "test";
